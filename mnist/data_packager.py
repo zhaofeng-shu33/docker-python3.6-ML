3692ec4cadfff0b56ab6a5ca6c8c88b4dc335a0d
zhaofeng-shu33 <616545598@qq.com>
2018-12-26 11:45:13 +0800

    add mnist test

---
+++ mnist/data_packager.py
@@ -0,0 +1,14 @@
+import numpy as np
+from sklearn.datasets import fetch_openml
+if __name__=="__main__":
+    X, y = fetch_openml('mnist_784', version=1, return_X_y=True)
+    X = X / 255.
+
+    # rescale the data, use the traditional train/test split
+    X_train, X_test = X[:60000], X[60000:]
+    y_train, y_test = y[:60000], y[60000:]
+
+    np.save('dist/X_train.npy', X_train)
+    np.save('X_test.npy', X_test)
+    np.save('dist/y_train.npy', y_train)
+    np.save('y_test.npy', y_test)
---
+++ mnist/run.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import traceback
+import os
+
+import numpy as np
+from sklearn import metrics
+import model
+
+def main():
+    '''load test data and calls the model predict function
+    '''
+    X_test = np.load('/app/X_test.npx')
+    y_test = np.load('/app/y_test.npx')
+    y_predict = model.predict(X_test)
+    return metrics.accuracy_score(y_test, y_predict)
+    
+if __name__=="__main__":
+    try:
+        acc = main()
+        print(acc)
+    except Exception as err:
+        print('Your code failed to run. Reason :')
+        print(err)
+        traceback.print_tb(err.__traceback__)
