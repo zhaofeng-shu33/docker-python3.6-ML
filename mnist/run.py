46eba22addabebdd7db4f803c8fa640e8eb6aa47
zhaofeng-shu33 <616545598@qq.com>
2018-12-26 17:04:56 +0800

    update run

--- mnist/run.py
+++ mnist/run.py
@@ -1,7 +1,9 @@
-#!/usr/bin/env python3
+#!python
 # -*- coding: utf-8 -*-
 import traceback
 import os
+import json
+import time
 
 import numpy as np
 from sklearn import metrics
@@ -12,14 +14,19 @@ def main():
     '''
     X_test = np.load('X_test.npy')
     y_test = np.load('y_test.npy')
+    start_time = time.time()
     y_predict = model.predict(X_test)
-    return metrics.accuracy_score(y_test, y_predict)
-    
+    elapsed_time = time.time() - start_time # in second
+    acc = metrics.accuracy_score(y_test, y_predict)
+    return {'time': elapsed_time, 'acc': acc}
 if __name__=="__main__":
     try:
-        acc = main()
-        print(acc)
+        result_json = main()
     except Exception as err:
         print('Your code failed to run. Reason :')
         print(err)
         traceback.print_tb(err.__traceback__)
+        exit(-1)
+    result_str = json.dumps(result_json)
+    with open('model/result.txt', 'w') as f:
+        f.write(result_str)
