52cdef5505a2a9c0fe93a2100f48391275bf81e2
zhaofeng-shu33 <616545598@qq.com>
2018-12-26 11:56:32 +0800

    mkdir dist

--- mnist/data_packager.py
+++ mnist/data_packager.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 from sklearn.datasets import fetch_openml
 if __name__=="__main__":
@@ -7,7 +8,8 @@ if __name__=="__main__":
     # rescale the data, use the traditional train/test split
     X_train, X_test = X[:60000], X[60000:]
     y_train, y_test = y[:60000], y[60000:]
-
+    if not(os.path.exists('dist')):
+        os.mkdir('dist')
     np.save('dist/X_train.npy', X_train)
     np.save('X_test.npy', X_test)
     np.save('dist/y_train.npy', y_train)
